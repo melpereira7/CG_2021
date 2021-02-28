1dbd9633e393ce6f1d72bec6bb0db36a58e75cc3
melpereira7 <melanipereira98@gmail.com>
2021-02-28 23:14:01 +0000

    First commit

---
+++ Aula1/code/main.cpp
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <stdio.h>
+
+float size = 1.0;
+float inRadius = 1.0;
+float outRadius = 1.0;
+float rings = 1.0;
+float sides = 1.0;
+float r = 0.2, g = 1.0, b = 0.8;
+
+void changeSize(int w, int h)
+{
+	// Prevent a divide by zero, when window is too short
+	// (you can�t make a window with zero width).
+	if (h == 0)
+		h = 1;
+	// compute window's aspect ratio
+	float ratio = w * 1.0f / h;
+	// Set the projection matrix as current
+	glMatrixMode(GL_PROJECTION);
+	// Load the identity matrix
+	glLoadIdentity();
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+	// Set the perspective
+	gluPerspective(45.0f, ratio, 1.0f, 1000.0f);
+	// return to the model view matrix mode
+	glMatrixMode(GL_MODELVIEW);
+}
+
+
+void renderScene(void)
+{
+	// clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	r += 0.01;
+	g += 0.04;
+	b += 0.07;
+	glColor3f((sin(r)+1)/2, (sin(g)+1)/2, (sin(b)+1)/2);
+
+	// set camera
+	glLoadIdentity();
+	gluLookAt(0.0f, 0.0f, 5.0f,
+			0.0f, 0.0f, -1.0f,
+			0.0f, 1.0f, 0.0f);
+		
+	// put drawing instructions here
+	size += 0.01;
+	glutWireTeapot(sin(size)+1);
+	
+	// End of frame
+	glutSwapBuffers();
+}
+
+
+
+void printInfo() {
+
+	printf("Vendor: %s\n", glGetString(GL_VENDOR));
+	printf("Renderer: %s\n", glGetString(GL_RENDERER));
+	printf("Version: %s\n", glGetString(GL_VERSION));
+}
+
+
+int main(int argc, char** argv)
+{
+	// put GLUT init here
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(800, 800);
+	glutCreateWindow("CG@DI");
+    
+	// put callback registry here
+	glutDisplayFunc(renderScene);
+	glutReshapeFunc(changeSize);
+	glutIdleFunc(renderScene);
+	
+	// some OpenGL settings
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	
+	// enter GLUT�s main cycle
+	glutMainLoop();
+	
+	return 1;
+}
